f396c840be0f080c5eaedd4383a7c427a51dedcf
Aldrin Oliveira <aldrin.o.247@gmail.com>
2018-06-30 15:32:55 +0530

    Angular Promises Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FormsModule } from '@angular/forms'; 
 import { ReactiveFormsModule } from '@angular/forms';
 import 'hammerjs';
@@ -60,6 +61,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatDialogModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule
